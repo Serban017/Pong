c4a8e997cf3e60d8ab30f863e4d5aebc755f7ad9
Serb <serbanargetoianu@gmail.com>
2024-09-06 13:44:33 +0300

    V1.2

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int player_score = 0; 
-int cpu_score = 0;
+int player_one_score = 0; 
+int player_two_score = 0;
 
 
 class Ball {
@@ -29,12 +29,12 @@ class Ball {
             
             if (x + radius >= GetScreenWidth())
             {
-                cpu_score++;
+                player_two_score++;
                ResetBall();
             }
             if (x - radius <= 0) 
             {
-                player_score++;
+                player_one_score++;
                 ResetBall();
             }
         }
@@ -50,29 +50,13 @@ class Ball {
         }
 };
 
-class Paddle {
-public:
-    float x, y;
-    float width, height;
-    int speed;
+class Player_One_Paddle {
 
-    void Draw() 
-    {
-        DrawRectangle(x, y, width, height, BLUE);
-    }
+protected: 
 
-    void Update() 
+    void Limit_Movement()
     {
-        if(IsKeyDown(KEY_W))
-        {
-            y = y - speed;
-        }
-        if (IsKeyDown(KEY_S))
-        {
-            y = y + speed;
-        }
-        
-        if (y <= 0) 
+        if (y <= 0)
         {
             y = 0;
         }
@@ -82,9 +66,33 @@ public:
         }
     }
 
-};
+    public:
+        float x, y;
+        float width, height;
+        int speed;
 
-class CPUPaddle : public Paddle {
+        void Draw() 
+        {
+            DrawRectangle(x, y, width, height, BLUE);
+        }
+
+        void Update() 
+        {
+            if(IsKeyDown(KEY_W))
+            {
+                y = y - speed;
+            }
+            if (IsKeyDown(KEY_S))
+            {
+                y = y + speed;
+            }
+            
+            Limit_Movement();
+        }
+
+    };
+
+class Player_Two_Paddle : public Player_One_Paddle {
     public: 
         void Update()
         {
@@ -97,21 +105,30 @@ class CPUPaddle : public Paddle {
                 y = y + speed;
             }
 
-            if (y <= 0)
-            {
-                y = 0;
-            }
-            if (y + height >= GetScreenHeight())
-            {
-                y = GetScreenHeight() - height;
-            }
+            Limit_Movement();
         }
 };
 
+class CPU_Paddle : public Player_One_Paddle {
+    public:
+        void Update(int ball_y)
+        {
+            if (y + height/2 > ball_y)
+            {
+                y = y - speed;
+            }
+            if(y + height/2 <= ball_y)
+            {
+                y = y + speed;
+            }
+            Limit_Movement();
+        }
+    };
 
 Ball ball;
-Paddle player;
-CPUPaddle cpu;
+Player_One_Paddle player_one;
+Player_Two_Paddle player_two;
+CPU_Paddle cpu;
 
 int main() {
 
@@ -127,17 +144,24 @@ int main() {
     ball.speed_x = 7;
     ball.speed_y = 7;
 
-    player.width = 25; 
-    player.height = 120;
-    player.x = 10;
-    player.y = (screen_height / 2) - (player.height / 2);
-    player.speed = 6; 
+    player_one.width = 25; 
+    player_one.height = 120;
+    player_one.x = 10;
+    player_one.y = (screen_height / 2) - (player_one.height / 2);
+    player_one.speed = 6; 
 
-    cpu.width = 25; 
+    player_two.width = 25; 
+    player_two.height = 120;
+    player_two.x = screen_width - player_two.width - 10;
+    player_two.y = screen_height / 2 - player_two.height / 2;
+    player_two.speed = 6;
+
+    cpu.width = 25;
     cpu.height = 120;
-    cpu.x = screen_width - cpu.width - 10;
-    cpu.y = screen_height / 2 - cpu.height / 2;
+    cpu.x = screen_width - player_two.width - 10;
+    cpu.y = screen_height / 2 - player_two.height / 2;
     cpu.speed = 6;
+
 
     while(WindowShouldClose() == false)
         {
@@ -145,27 +169,33 @@ int main() {
         
         //Updating ball position, player and cpu
         ball.Update();
-        player.Update();
-        cpu.Update();
-        
+        player_one.Update();
+        //player_two.Update();
+        cpu.Update(ball.y);
         //Checking for colision
-        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ player.x, player.y, player.width, player.height })) 
+        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ player_one.x, player_one.y, player_one.width, player_one.height })) 
         {
             ball.speed_x *= -1;
         }
         
+        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ player_two.x, player_two.y, player_two.width, player_two.height }))
+        {
+            ball.speed_x *= -1;
+        }
+
         if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ cpu.x, cpu.y, cpu.width, cpu.height }))
         {
             ball.speed_x *= -1;
         }
-        //Drawing the ball and paddles
+        //Drawing the ball, paddles, line and score
         ClearBackground(BLACK);
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
         ball.Draw();
-        player.Draw();
+        player_one.Draw();
+        //player_two.Draw();
         cpu.Draw();
-        DrawText(TextFormat("%i",cpu_score), screen_width/4 - 20, 20, 80, WHITE);
-        DrawText(TextFormat("%i", player_score),3 * screen_width / 4 - 20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", player_two_score), screen_width/4 - 20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", player_one_score),3 * screen_width / 4 - 20, 20, 80, WHITE);
         EndDrawing();
         }
 
